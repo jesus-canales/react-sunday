d83ee93928e6046932d9d91f02d22571cf3cb443
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-18 14:04:43 +0000

    He realizado cambios en el archivo App.jsx

--- HelloWorld/src/App.jsx
+++ HelloWorld/src/App.jsx
@@ -15,8 +15,11 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Cibertec</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +29,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Clic en Vite, React y Cibertec para saber más...
       </p>
     </>
   )
