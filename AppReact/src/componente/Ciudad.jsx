504b4391678312e8a2bf816f445d0de030bb3a16
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-18 16:26:06 +0000

    He creado un componente con parámetros: Ciudad

--- AppReact/src/componente/Ciudad.jsx
+++ AppReact/src/componente/Ciudad.jsx
@@ -1,10 +1,15 @@
 import { Fragment } from "react";
+import './EstilosCiudad.css'
 
-function Ciudad (){
+function Ciudad ( {nombre, descripcion, imagen} ){
 
     return (
         <>
-
+        <div className="ciudad">
+            <h3>{nombre}</h3>
+            <p>{descripcion}</p>
+            <img src={imagen} alt={nombre} />
+        </div>
         </>
     );
 }
