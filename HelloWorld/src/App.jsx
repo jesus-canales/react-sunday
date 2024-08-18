aec2204320878cc55bf43536cc838f09ce008976
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-18 14:16:39 +0000

    He vinculado una imagen interna y además he modificado la ruta de vínculo a cibertec.edu.pe

--- HelloWorld/src/App.jsx
+++ HelloWorld/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import ciberteclogo from './image/ciberteclogo.png'
 import './App.css'
 
 function App() {
@@ -15,8 +16,8 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://www.cibertec.edu.pe/" target="_blank">
+          <img src={ciberteclogo} className="logo" alt="Vite logo" />
         </a>
       </div>
       <h1>Vite + React + Cibertec</h1>
