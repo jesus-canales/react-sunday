9c65c47ae49a418547b9a3054ae9e71978693b2f
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-18 16:33:20 +0000

    He reutilizado el componente ciudad pasándole los parámetros

--- AppReact/src/App.jsx
+++ AppReact/src/App.jsx
@@ -12,6 +12,10 @@ function App() {
       nombre="Lima"
       descripcion="La capital del Perú"
       imagen="https://www.peru.travel/contenido/general/imagen/es/643/1.1/lima-plaza-de-armas-consejos.jpg" />
+      <Ciudad
+      nombre="Arequipa"
+      descripcion="Encatadora tierra volcánica"
+      imagen="https://www.civitatis.com/blog/wp-content/uploads/2023/02/panoramica-arequipa-peru.jpg" />
       <Pie />
     </>
   );
